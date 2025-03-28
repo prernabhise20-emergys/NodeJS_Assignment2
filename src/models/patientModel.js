e6a9c084abd7eba5f79f1196a99c5fa1085a05c9
prernabhise20-emergys <prerna.bhise@emergys.com>
2025-03-28 10:06:10 +0530

    updated

--- src/models/patientModel.js
+++ src/models/patientModel.js
@@ -1,9 +1,6 @@
 import db from "../db/connection.js";
 import { AUTH_RESPONSES } from "../common/constants/response.js";
-const {
-  UNAUTHORIZED_ACCESS,
-} = AUTH_RESPONSES;
-
+const { UNAUTHORIZED_ACCESS } = AUTH_RESPONSES;
 
 const getInfo = async (is_admin, limit, offset) => {
   try {
@@ -689,52 +686,54 @@ const removeDocument = (patient_id, document_type) => {
 
 const ageGroupWiseData = (is_admin) => {
   return new Promise((resolve, reject) => {
-    if(is_admin){
-    db.query(
-      `
-      SELECT 
-        COUNT(age) as count,
-        CASE 
-          WHEN age BETWEEN 0 AND 12 THEN 'child'
-          WHEN age BETWEEN 13 AND 18 THEN 'teen'
-          WHEN age BETWEEN 19 AND 60 THEN 'adult'
-          WHEN age > 60 THEN 'older'
-        END as ageGroup
-       FROM personal_info p 
-        JOIN user_register r ON p.user_id = r.id 
-        JOIN family_info f ON f.patient_id = p.patient_id 
-        JOIN disease d ON d.patient_id = p.patient_id 
-        JOIN documents do ON do.patient_id = p.patient_id 
-        WHERE p.is_deleted = false 
-          AND f.is_deleted = false 
-          AND d.is_deleted = false 
-          AND do.is_deleted = false 
+    if (is_admin) {
+      db.query(
+        `
+     SELECT 
+    COUNT(age) AS count,
+    CASE 
+        WHEN age BETWEEN 0 AND 12 THEN 'child'
+        WHEN age BETWEEN 13 AND 18 THEN 'teen'
+        WHEN age BETWEEN 19 AND 60 THEN 'adult'
+        WHEN age > 60 THEN 'older'
+    END AS ageGroup
+FROM personal_info p 
+JOIN user_register r ON p.user_id = r.id 
+JOIN family_info f ON f.patient_id = p.patient_id 
+JOIN disease d ON d.patient_id = p.patient_id 
+JOIN documents do ON do.patient_id = p.patient_id 
+WHERE p.is_deleted = false 
+  AND f.is_deleted = false 
+  AND d.is_deleted = false 
+  AND do.is_deleted = false 
+GROUP BY ageGroup;
     `,
-      (error, result) => {
-        if (error) {
-          return reject(error);
+        (error, result) => {
+          if (error) {
+            return reject(error);
+          }
+          resolve(result);
         }
-        resolve(result);
-      }
-    );
-  }
-  else{
-    throw UNAUTHORIZED_ACCESS;
-  }
+      );
+    } else {
+      throw UNAUTHORIZED_ACCESS;
+    }
   });
-
 };
 const getDocumentByPatientIdAndType = async (patient_id, document_type) => {
   return new Promise((resolve, reject) => {
-    db.query('SELECT * FROM documents WHERE patient_id = ? AND document_type = ? LIMIT 1', [patient_id, document_type], (err, results) => {
-      if (err) {
-        return reject(err);
+    db.query(
+      "SELECT * FROM documents WHERE patient_id = ? AND document_type = ? LIMIT 1",
+      [patient_id, document_type],
+      (err, results) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(results[0]);
       }
-      resolve(results[0]);
-    });
+    );
   });
 };
-
 
 // const getDocumentByPatientIdAndType = async (patient_id, document_type) => {
 //   return new Promise((resolve, reject) => {
@@ -744,11 +743,10 @@ const getDocumentByPatientIdAndType = async (patient_id, document_type) => {
 //       if (err) {
 //         return reject(err);
 //       }
-//       resolve(results[0]); 
+//       resolve(results[0]);
 //     });
 //   });
 // };
-
 
 export {
   getDocumentByPatientIdAndType,
@@ -777,5 +775,5 @@ export {
   updateDiseaseDetails,
   getUploadInfo,
   deleteDiseaseDetails,
-  getTotalCount
+  getTotalCount,
 };
